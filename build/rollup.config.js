8cf17a889ab13e8fb53e2d34f871b6da77a02dd0
Kendall Johnson <kenjohnson1994@gmail.com>
2022-08-08 19:31:20 -0500

    Update package references

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,8 +1,8 @@
 import vue from 'rollup-plugin-vue';
-import buble from 'rollup-plugin-buble';
+import buble from '@rollup/plugin-buble';
 import cleanup from 'rollup-plugin-cleanup';
 import { terser } from 'rollup-plugin-terser';
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
 
 const isMinify = process.env.BUILD_MODE === 'minify';
 
